cafc2e709a730fd59e778c1766cc14b9004cdc0f
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-01-28 23:02:07 -0500

    Load the xml file

--- src/main/java/ntrpg/gui/GuiListener.java
+++ src/main/java/ntrpg/gui/GuiListener.java
@@ -2,9 +2,10 @@ package ntrpg.gui;
 
 import cz.neumimto.core.ioc.Singleton;
 import io.github.minecraftgui.models.components.UserGui;
+import io.github.minecraftgui.models.factories.GuiFactory;
 import io.github.minecraftgui.models.listeners.OnGuiListener;
 
-import java.util.UUID;
+import java.io.File;
 
 /**
  * Created by Nt on 25.01.2016.
@@ -13,12 +14,15 @@ import java.util.UUID;
 @Singleton
 public class GuiListener implements OnGuiListener{
 
+    //You should change the file location where you will put it.
+    private GuiFactory.GuiModel guiModel = GuiFactory.createGuiModel(new File("index.xml"));
 
     public void onGuiPreInit(UserGui userGui) {
-
+        guiModel.preInitGui(MinecraftModGuiService.minecraftGuiService.getPluginInterface(), userGui);
     }
 
     public void onGuiInit(UserGui userGui) {
-
+        guiModel.initGui(MinecraftModGuiService.minecraftGuiService.getPluginInterface(), userGui, this);
     }
+
 }
