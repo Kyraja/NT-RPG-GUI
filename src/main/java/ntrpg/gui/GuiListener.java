d43d41923c4e2cb85fe681cd1ea00cce9edf0bd6
NeumimTo <chleba48@gmail.com>
2016-01-31 12:02:53 +0100

    update

--- src/main/java/ntrpg/gui/GuiListener.java
+++ src/main/java/ntrpg/gui/GuiListener.java
@@ -1,28 +1,39 @@
 package ntrpg.gui;
 
+import cz.neumimto.NtRpgPlugin;
+import cz.neumimto.core.ioc.Inject;
 import cz.neumimto.core.ioc.Singleton;
+import cz.neumimto.events.PlayerGuiModInitEvent;
 import io.github.minecraftgui.models.components.UserGui;
 import io.github.minecraftgui.models.factories.GuiFactory;
 import io.github.minecraftgui.models.listeners.OnGuiListener;
+import org.spongepowered.api.Game;
 
 import java.io.File;
+import java.util.UUID;
 
 /**
  * Created by Nt on 25.01.2016.
  */
-
 @Singleton
 public class GuiListener implements OnGuiListener{
 
-    //You should change the file location where you will put it.
-    private GuiFactory.GuiModel guiModel = GuiFactory.createGuiModel(new File("index.xml"));
+    private GuiFactory.GuiModel guiModel = GuiFactory.createGuiModel(new File(NtRpgPlugin.workingDir,"index.xml"));
+
+    @Inject
+    private Game game;
 
     public void onGuiPreInit(UserGui userGui) {
         guiModel.preInitGui(MinecraftModGuiService.minecraftGuiService.getPluginInterface(), userGui);
     }
 
     public void onGuiInit(UserGui userGui) {
+
         guiModel.initGui(MinecraftModGuiService.minecraftGuiService.getPluginInterface(), userGui, this);
+
+        UUID playerUUID = userGui.getPlayerUUID();
+        PlayerGuiModInitEvent event = new PlayerGuiModInitEvent(playerUUID);
+        game.getEventManager().post(event);
     }
 
 }
--- src/main/java/ntrpg/gui/GuiModMessaging.java
+++ src/main/java/ntrpg/gui/GuiModMessaging.java
@@ -3,6 +3,7 @@ package ntrpg.gui;
 import com.google.inject.Singleton;
 import cz.neumimto.effects.EffectStatusType;
 import cz.neumimto.effects.IEffect;
+import cz.neumimto.gui.Gui;
 import cz.neumimto.gui.IPlayerMessage;
 import cz.neumimto.players.CharacterBase;
 import cz.neumimto.players.ExtendedNClass;
@@ -28,27 +29,27 @@ public class GuiModMessaging implements IPlayerMessage {
     }
 
     public void sendMessage(IActiveCharacter iActiveCharacter, String s) {
-
+        iActiveCharacter.sendMessage(s);
     }
 
     public void openSkillTreeMenu(IActiveCharacter iActiveCharacter, SkillTree skillTree, Map<String, Integer> map) {
-
+        Gui.vanilla.openSkillTreeMenu(iActiveCharacter,skillTree,map);
     }
 
     public void moveSkillTreeMenu(IActiveCharacter iActiveCharacter, SkillTree skillTree, Map<String, Integer> map, SkillData skillData) {
-
+        Gui.vanilla.moveSkillTreeMenu(iActiveCharacter,skillTree,map,skillData);
     }
 
     public void sendEffectStatus(IActiveCharacter iActiveCharacter, EffectStatusType effectStatusType, IEffect iEffect) {
-
+        Gui.vanilla.sendEffectStatus(iActiveCharacter,effectStatusType,iEffect);
     }
 
     public void invokeCharacterMenu(Player player, List<CharacterBase> list) {
-
+        Gui.vanilla.invokeCharacterMenu(player,list);
     }
 
     public void sendCooldownMessage(IActiveCharacter iActiveCharacter, String s, double v) {
-
+        Gui.vanilla.sendCooldownMessage(iActiveCharacter,s,v);
     }
 
     public void sendManaStatus(IActiveCharacter iActiveCharacter, double currentMana, double maxMana, double reserved) {
@@ -62,26 +63,26 @@ public class GuiModMessaging implements IPlayerMessage {
     }
 
     public void sendPlayerInfo(IActiveCharacter iActiveCharacter, List<CharacterBase> list) {
-
+        Gui.vanilla.sendPlayerInfo(iActiveCharacter,list);
     }
 
     public void sendPlayerInfo(IActiveCharacter iActiveCharacter, IActiveCharacter iActiveCharacter1) {
-
+        Gui.vanilla.sendPlayerInfo(iActiveCharacter,iActiveCharacter1);
     }
 
     public void showExpChange(IActiveCharacter iActiveCharacter, String s, double v) {
-
+        Gui.vanilla.showExpChange(iActiveCharacter,s,v);
     }
 
     public void showLevelChange(IActiveCharacter iActiveCharacter, ExtendedNClass extendedNClass, int i) {
-
+        Gui.vanilla.showLevelChange(iActiveCharacter,extendedNClass,i);
     }
 
     public void sendStatus(IActiveCharacter iActiveCharacter) {
-
+        Gui.vanilla.sendStatus(iActiveCharacter);
     }
 
     public void showAvalaibleClasses(IActiveCharacter iActiveCharacter) {
-
+        Gui.vanilla.showAvalaibleClasses(iActiveCharacter);
     }
 }
